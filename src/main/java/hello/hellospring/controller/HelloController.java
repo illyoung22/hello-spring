c4399ea1cacf50208358d6e97308b73281eb3e14
illyoung22 <illyoung22@hanmail.net>
2023-09-07 11:40:52 +0900

    김영한 강의 최초커밋

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,30 @@
+package hello.hellospring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        UUID uuid = UUID.randomUUID();
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return  "hello " + name;
+    }
+}
